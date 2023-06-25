2da44132e5b061e00136742fa8b7925773b135c3
Ankit Kumar Pandey <itsankitkp@gmail.com>
2023-06-25 16:32:49 +0530

    move defintion elsewhere

--- io.c
+++ io.c
@@ -1,17 +1,5 @@
 #include "io.h"
-/* The I/O ports */
-#define FB_COMMAND_PORT 0x3D4
-#define FB_DATA_PORT 0x3D5
-/* The I/O port commands */
-#define FB_HIGH_BYTE_COMMAND 14
-#define FB_LOW_BYTE_COMMAND 15
 
-/* VGA provides support for 16 colors */
-#define BLACK 0
-#define GREEN 2
-#define RED 4
-#define YELLOW 14
-#define WHITE 15
 /** fb_move_cursor:
  * Moves the cursor of the framebuffer to the given position
  *

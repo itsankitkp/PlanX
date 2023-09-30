1746cf5492416756f0b29debd12726fcaa5cc3ca
Ankit Kumar Pandey <itsankitkp@gmail.com>
2023-09-30 17:43:09 +0530

    modify linker and loader

--- arch/i386/loader.s
+++ arch/i386/loader.s
@@ -5,18 +5,24 @@ ALIGN_MODULES equ 0x00000001
 
 ; calculate the checksum (all options + checksum should equal 0)
 CHECKSUM equ -(MAGIC_NUMBER + ALIGN_MODULES) 
-section .multiboot               ;according to multiboot spec
+section .multiboot.data               ;according to multiboot spec
         dd MAGIC_NUMBER           ;set magic number for
                                  ;bootloader
         dd ALIGN_MODULES         ;set flags
         dd CHECKSUM  ;set checksum
 
-section .text
+
 global start
 extern main                      ;defined in the C file
 
+section .multiboot.text
 start:
         cli                      ;block interrupts
+        lea ebx, [higher_half] ; load the address of the label in ebx
+        jmp ebx ; jump to the label
+
+section .text
+higher_half:
         mov esp, stack_space     ;set stack pointer
         mov ebp, stack_space     ;set stack pointer
         push ebx

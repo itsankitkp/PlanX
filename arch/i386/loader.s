3931f50cb6c5594419496dd8b10d0fef260486c8
Ankit Kumar Pandey <itsankitkp@gmail.com>
2023-10-01 15:17:51 +0530

    minor changes

--- arch/i386/loader.s
+++ arch/i386/loader.s
@@ -25,17 +25,48 @@ page_directory:
     ; bit 7: PS The kernel page is 4MB.
     ; bit 1: RW The kernel page is read/write.
     ; bit 0: P  The kernel page is present.
-    dd (page_table1<<12) | 0x83; first table
+    dd 0x00000083; first table
     times (768-1) dd 0; rest of the tables are empty
-    dd (page_table768<<12) | 0x83 ; 768th table for 0xC0000000
-    times (1024 - 768 - 1) dd 0;  rest of entries unmapped again
+    dd 0x00000083 ; 768th table for 0xC0000000
+    times 1024 - 768 - 1 dd 0;  rest of entries unmapped again
+
+; GDT (Global Descriptor Table) Setup
+gdt_start:
+    dq 0x0  ; Null descriptor
+    dq 0x0000000000000000  ; First descriptor (null)
+    
+    ; Code segment descriptor for accessing the first 1 MB (Limit = 1 MB)
+    dq 0x00CF920000000000  ; Base = 0x0, Limit = 1 MB, Code segment
+    
+    ; Data segment descriptor for accessing the first 1 MB (Limit = 1 MB)
+    dq 0x00CF960000000000  ; Base = 0x0, Limit = 1 MB, Data segment
+
+    ; Code segment descriptor for the higher-half kernel (Base = 0xC0000000, Limit = 4 GB)
+    dq 0x0020980000000000  ; Code segment descriptor (Base = 0xC0000000, Limit = 4 GB)
+
+    ; Data segment descriptor for the higher-half kernel (Base = 0xC0000000, Limit = 4 GB)
+    dq 0x0000920000000000  ; Data segment descriptor (Base = 0xC0000000, Limit = 4 GB)
+
+gdt_end:
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
 
 global start
 extern main                      ;defined in the C file
 
 section .multiboot.text
 start:
-        mov ecx, page_table
+
+        xor eax, eax
+        lgdt  [gdt_descriptor]
+        
+        mov eax, cr0 ; Enable paging
+        or eax, 0x80000001
+        mov cr0, eax
+
+        mov ecx, page_directory
         mov cr3, ecx ;
         mov ebx, cr4 ; read current cr4
         or ebx, 0x00000010 ; set PSE

62ea7f2b402565a99107ca9d854053b269bd5ce2
Ankit Kumar Pandey <itsankitkp@gmail.com>
2023-12-23 17:49:16 +0530

    pd can now be used for task switch

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -15,45 +15,64 @@ void run_modules(multiboot_info_t *mb_info)
     multiboot_uint32_t mods_count = mb_info->mods_count; /* Get the amount of modules available */
 
     multiboot_uint32_t mods_addr = mb_info->mods_addr; /* And the starting address of the modules */
-
+    process_t *p1, *p2;
     for (u32int mod = 0; mod < mods_count; mod++)
     {
         multiboot_module_t *module = (multiboot_module_t *)(mods_addr + (mod * sizeof(multiboot_module_t))); /* Loop through all modules */
         call_module_t start_program = (call_module_t)module->mod_start;
-
-        if (start_program)
+        if (mod == 0)
         {
-            u32int size = (call_module_t)module->mod_end-(call_module_t)module->mod_start;
-            process_t* p = kmalloc_a(sizeof(process_t), TRUE);
-            init_user_space_paging(p);
-            // __asm__ ("int $0x80");
-            
-            invlpg(0);
-            invlpg(0x1000000);
-            invlpg(0xC0000000);
-            memcpy(0, (char*) start_program, size);
-            enter_user_mode();
-       
+            if (start_program)
+            {
+                u32int size = (call_module_t)module->mod_end - (call_module_t)module->mod_start;
+                p1 = (process_t *)kmalloc_a(sizeof(process_t), TRUE);
+                init_user_space_paging(p1);
+                // __asm__ ("int $0x80");
 
+                invlpg(0);
+                invlpg(0xC0000000);
+                memcpy(0, (char *)start_program, size);
+                // enter_user_mode();
+            }
+        }
+        else
+        {
+            if (start_program)
+            {
+                u32int size = (call_module_t)module->mod_end - (call_module_t)module->mod_start;
+                p2 = (process_t *)kmalloc_a(sizeof(process_t), TRUE);
+                init_user_space_paging(p2);
+                // __asm__ ("int $0x80");
+
+                invlpg(0);
+                invlpg(0xC0000000);
+                memcpy(0, (char *)start_program, size);
+                // enter_user_mode();
+            }
         }
     }
+    u32int page_directory_phy_addr = (u32int)&p2->pd - 0xC0000000;
+    loadPageDirectory(page_directory_phy_addr);
+    enter_user_mode();
 }
 
 int main(const void *multiboot_struct)
 {
     fb_clear_screen();
     init_descriptor_tables();
-    
+
     fb_write("System is up\0", WHITE);
     tss_init(5, 0x10, 0x10);
-    if (!multiboot_struct){}
+    if (!multiboot_struct)
+    {
+    }
     init_paging();
-    
-//     enable_page(0x00000000, 10);
-//     enable_page(0x10000000, 10);
-//     enable_page(0xA0000000, 1);
-//      u32int ptr = *(u32int*)0xA0000000;
-// ptr++;
-//     return 0;
- run_modules((multiboot_info_t *)multiboot_struct);
+
+    //     enable_page(0x00000000, 10);
+    //     enable_page(0x10000000, 10);
+    //     enable_page(0xA0000000, 1);
+    //      u32int ptr = *(u32int*)0xA0000000;
+    // ptr++;
+    //     return 0;
+    run_modules((multiboot_info_t *)multiboot_struct);
 }
